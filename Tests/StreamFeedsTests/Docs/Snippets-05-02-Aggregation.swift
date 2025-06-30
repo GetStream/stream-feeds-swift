c166500f3a7c74c26540a73ce9f9cf0eb70022cb
Toomas Vahter <toomas.vahter@getstream.io>
2025-06-30 15:26:59 +0300

    Add snippets for aggregation

---
+++ Tests/StreamFeedsTests/Docs/Snippets-05-02-Aggregation.swift
@@ -0,0 +1,24 @@
+//
+// Copyright © 2025 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+import StreamFeeds
+
+@MainActor struct Snippets_05_02_Aggregation {
+    var client: FeedsClient!
+    var notificationFeed: Feed!
+    
+    func dataFormat() async throws {
+        // Mark all activities in the feed as seen
+        try await notificationFeed.markActivity(
+            request: .init(markAllSeen: true)
+        )
+        // Mark some activities as read via specific Activity Group Ids
+        try await notificationFeed.markActivity(
+            request: .init(
+                markRead: ["activityGroupIdOne", "activityGroupIdTwo"]
+            )
+        )
+    }
+}
