d5c5d474fbf3a939d38e062d622c67b770b8a3f9
Alexey Alter-Pesotskiy <alex@testableapple.com>
2025-07-08 12:00:54 +0100

    ⚙️ Implement CI (#1)

---
+++ Sources/StreamFeeds/Utils/SystemEnvironment+Version.swift
@@ -0,0 +1,11 @@
+//
+// Copyright © 2025 Stream.io Inc. All rights reserved.
+//
+// ⚠️  Generated file, please use `fastlane release major|minor|patch` lane
+
+import Foundation
+
+extension SystemEnvironment {
+  /// A Stream Feeds version.
+  public static let version: String = "0.0.1-SNAPSHOT"
+}
