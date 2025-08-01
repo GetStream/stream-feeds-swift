2b2ac0617463d68d16dfb23388d847cbd69d951e
Alexey Alter-Pesotskiy <alex@testableapple.com>
2025-08-01 09:28:39 +0100

    [CI] Update swiftformat paths (#11)

--- Scripts/generate.sh
+++ Scripts/generate.sh
@@ -8,4 +8,4 @@ rm -rf $OUTPUT_DIR_FEEDS
 
 ( cd $CHAT_DIR ; make openapi ; go run ./cmd/chat-manager openapi generate-client --language swift --spec ./releases/v2/feeds-clientside-api.yaml --output $OUTPUT_DIR_FEEDS )
 
-mint run swiftformat Sources/StreamFeeds/generated/feeds/
+mint run swiftformat --config .swiftformat Sources/StreamFeeds/generated/feeds/
