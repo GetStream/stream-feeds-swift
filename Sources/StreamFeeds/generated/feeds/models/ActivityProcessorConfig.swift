d0ac2da1712c4f5858440e34914ba81de3137c11
Martin Mitrevski <martinmitrevski.oh@gmail.com>
2025-08-14 17:59:47 +0200

    Latest codegen

--- Sources/StreamFeeds/generated/feeds/models/ActivityProcessorConfig.swift
+++ Sources/StreamFeeds/generated/feeds/models/ActivityProcessorConfig.swift
@@ -7,25 +7,30 @@ import StreamCore
 
 public final class ActivityProcessorConfig: @unchecked Sendable, Codable, JSONEncodable, Hashable {
     public var config: [String: RawJSON]?
+    public var openaiKey: String?
     public var type: String
 
-    public init(config: [String: RawJSON]? = nil, type: String) {
+    public init(config: [String: RawJSON]? = nil, openaiKey: String? = nil, type: String) {
         self.config = config
+        self.openaiKey = openaiKey
         self.type = type
     }
 
     public enum CodingKeys: String, CodingKey, CaseIterable {
         case config
+        case openaiKey = "openai_key"
         case type
     }
 
     public static func == (lhs: ActivityProcessorConfig, rhs: ActivityProcessorConfig) -> Bool {
         lhs.config == rhs.config &&
+            lhs.openaiKey == rhs.openaiKey &&
             lhs.type == rhs.type
     }
 
     public func hash(into hasher: inout Hasher) {
         hasher.combine(config)
+        hasher.combine(openaiKey)
         hasher.combine(type)
     }
 }
--- Sources/StreamFeeds/generated/feeds/models/AggregationConfig.swift
+++ Sources/StreamFeeds/generated/feeds/models/AggregationConfig.swift
@@ -7,20 +7,25 @@ import StreamCore
 
 public final class AggregationConfig: @unchecked Sendable, Codable, JSONEncodable, Hashable {
     public var format: String?
+    public var groupSize: Int?
 
-    public init(format: String? = nil) {
+    public init(format: String? = nil, groupSize: Int? = nil) {
         self.format = format
+        self.groupSize = groupSize
     }
 
     public enum CodingKeys: String, CodingKey, CaseIterable {
         case format
+        case groupSize = "group_size"
     }
 
     public static func == (lhs: AggregationConfig, rhs: AggregationConfig) -> Bool {
-        lhs.format == rhs.format
+        lhs.format == rhs.format &&
+            lhs.groupSize == rhs.groupSize
     }
 
     public func hash(into hasher: inout Hasher) {
         hasher.combine(format)
+        hasher.combine(groupSize)
     }
 }
