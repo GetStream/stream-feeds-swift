a2175a0a7abc5730e6a72f3c12a9720b3b8d2265
Alexey Alter-Pesotskiy <alex@testableapple.com>
2025-09-19 16:53:38 +0100

    Bump 0.1.3

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/GetStream/stream-core-swift.git", exact: "0.1.2")
+        .package(url: "https://github.com/GetStream/stream-core-swift.git", exact: "0.1.3")
     ],
     targets: [
         .target(
