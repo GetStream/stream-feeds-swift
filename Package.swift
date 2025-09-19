3a962de5305459e8c0d9a95cfba823f62283199e
Alexey Alter-Pesotskiy <alex@testableapple.com>
2025-09-19 15:47:27 +0100

    Bump stream-core version

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/GetStream/stream-core-swift.git", branch: "develop")
+        .package(url: "https://github.com/GetStream/stream-core-swift.git", exact: "0.1.0")
     ],
     targets: [
         .target(
