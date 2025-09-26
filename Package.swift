30fb5b46da9437e7d468441c5122f6de1aa6986c
Toomas Vahter <toomas.vahter@getstream.io>
2025-09-26 15:37:31 +0300

    Use StreamCore 0.2.1

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/GetStream/stream-core-swift.git", exact: "0.2.0")
+        .package(url: "https://github.com/GetStream/stream-core-swift.git", exact: "0.2.1")
     ],
     targets: [
         .target(
